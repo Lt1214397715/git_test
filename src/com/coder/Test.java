95964530f24438087c9d680649fc71d7cc05b173
lt <1214397715@qq.com>
2024-04-26 15:39:41 +0800

    取消证书测试推送到gitee1

--- src/com/coder/Test.java
+++ src/com/coder/Test.java
@@ -18,5 +18,6 @@ public class Test {
         System.out.println("hello gitee web");
         System.out.println("测试推送到gitee");
         System.out.println("取消gitee的证书，尝试推送到gitee");
+        System.out.println("取消gitee的证书，尝试推送到gitee");
     }
 }
