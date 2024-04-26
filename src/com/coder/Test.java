d168af95a3cf74ef1d984d02aca38ec738707a1d
lt <1214397715@qq.com>
2024-04-26 11:03:54 +0800

    第一次提交

---
+++ src/com/coder/Test.java
@@ -0,0 +1,12 @@
+package com.coder;
+
+/**
+ * @author LT
+ * @date 2024/4/26
+ * @project git_test
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
