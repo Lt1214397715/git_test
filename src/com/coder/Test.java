4411bd9dc78b7e468f6a542f376e5720e5e46923
池鱼 <159247631+Lt1214397715@users.noreply.github.com>
2024-04-26 13:27:17 +0800

    Update Test.java
    github web commit

--- src/com/coder/Test.java
+++ src/com/coder/Test.java
@@ -13,5 +13,6 @@ public class Test {
         System.out.println("hello dev");
         System.out.println("hello fix");
         System.out.println("hello github");
+        System.out.println("hello github web");
     }
 }
